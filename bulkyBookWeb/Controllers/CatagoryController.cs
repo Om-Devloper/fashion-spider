d123a9e372e96e5fc426faac014c8a02f1c6694b
prathamesh <pandyaom122@gmail.com>
2022-09-13 23:37:45 +0530

    showing data table in wed page

--- bulkyBookWeb/Controllers/CatagoryController.cs
+++ bulkyBookWeb/Controllers/CatagoryController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using bulkyBookWeb.Data;
+using bulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace bulkyBookWeb.Controllers
 {
+    
+
     public class CatagoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CatagoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Catagory> objCatagoryList = _db.Catagories;
+            return View(objCatagoryList); 
         }
     }
 }
