6d0b0c08d88684f8effea549e55df91388111bf0
prathamesh <pandyaom122@gmail.com>
2022-12-27 00:53:11 +0530

    connection Done with new classes

--- bulkyBookWeb/Controllers/HomeController.cs
+++ bulkyBookWeb/Controllers/HomeController.cs
@@ -16,45 +16,19 @@ namespace bulkyBookWeb.Controllers
         {
             _logger = logger;
         }
-
-        //private readonly crawlerDbContext _db;
-
-        //public HomeController(crawlerDbContext db)
-        //{
-        //    _db = db;
-        //}
-
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
         public IActionResult tommyhilfiger() 
         {
-            scrapper();
+            //scrapper();
             return View();
         }
-
-        ////Get
-        //public IActionResult Edit(int? id)
-        //{
-        //    return View(cnn);
-        //}
-        ////Post
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-
-        //public IActionResult Edit(Catagory obj)
-        //{
-        //    _db.Catagories.Update(obj);
-        //    _db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
@@ -63,6 +37,7 @@ namespace bulkyBookWeb.Controllers
         public static void scrapper()
         {
             tommyHilfiger.tommyhilfiger_Data_Process(0,1);
+
         }
     }
 }
---
+++ bulkyBookWeb/Models/connectionWithWeb.cs
@@ -0,0 +1,40 @@
+﻿using ConnectionLibrary;
+using System.Data;
+
+namespace bulkyBookWeb.Models.connectWithWeb
+{
+
+    public class connectionWithWeb
+    {
+        public static ConnectionClass Con = new ConnectionClass();
+        public connectionWithWeb()
+        {
+        }
+        public int Count()
+        {
+             var totalCount = Con.Select("SELECT * FROM [productDataTable]").Rows.Count;
+            return totalCount;
+        }
+        public string productDetail(int i)
+        {
+            var printProduct = Con.Select("SELECT productDetail FROM [productDataTable]").Rows[i].ItemArray.FirstOrDefault().ToString();
+            return printProduct;
+        }
+        public string productValue(int i)
+        {
+            var productValue = Con.Select("SELECT productValue FROM [productDataTable]").Rows[i].ItemArray.FirstOrDefault().ToString();
+            return productValue;
+        }
+        public string imageUrl(int i)
+        {
+            var imageUrl = Con.Select("SELECT imageUrl FROM [productDataTable]").Rows[i].ItemArray.FirstOrDefault().ToString();
+            return imageUrl;
+        }
+        public string productUrl(int i)
+        {
+            var productUrl = Con.Select("SELECT productUrl FROM [productDataTable]").Rows[i].ItemArray.FirstOrDefault().ToString();
+            return productUrl;
+        }
+
+    }
+}
